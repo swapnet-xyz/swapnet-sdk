4ccbd0b1aa8e8fba557ba662b5cf1be6120c5f1a
Xinxing Liu <xinxingliu86@gmail.com>
2024-11-16 02:08:40 +0000

    clean up

--- src/common/universalRouterDeployments.ts
+++ src/common/universalRouterDeployments.ts
@@ -1,48 +1,6 @@
 
 import { ChainId, LiquiditySourceUname } from "./unames.js";
 
-
-// // move to Swap API app configs
-// export const swapApiSupportedLiquiditySourcesByChainId: Record<ChainId, LiquiditySourceUname []> = {
-//     [ChainId.EthereumMainnet]: [
-//         LiquiditySourceUname.UniswapV2,
-//         LiquiditySourceUname.UniswapV3,
-//         LiquiditySourceUname.PancakeswapV3,
-//         LiquiditySourceUname.CurveV1,
-//         LiquiditySourceUname.Clipper,
-//     ],
-//     [ChainId.ArbitrumOne]: [
-//         LiquiditySourceUname.UniswapV2,
-//         LiquiditySourceUname.UniswapV3,
-//         LiquiditySourceUname.PancakeswapV3,
-//         LiquiditySourceUname.SushiswapV2,
-//         LiquiditySourceUname.SushiswapV3,
-//         LiquiditySourceUname.CurveV1,
-//         LiquiditySourceUname.BebopOrderbook,
-//     ],
-//     [ChainId.BaseMainnet]: [
-//         LiquiditySourceUname.Aerodrome,
-//         LiquiditySourceUname.UniswapV2,
-//         LiquiditySourceUname.UniswapV3,
-//         LiquiditySourceUname.PancakeswapV3,
-//         LiquiditySourceUname.SushiswapV2,
-//         LiquiditySourceUname.SushiswapV3,
-//         LiquiditySourceUname.BebopOrderbook,
-//     ],
-//     [ChainId.BlastMainnet]: [
-//         LiquiditySourceUname.ThrusterV2_3k,
-//         LiquiditySourceUname.ThrusterV2_10k,
-//         LiquiditySourceUname.ThrusterV3,
-//         LiquiditySourceUname.Blasterswap,
-//         LiquiditySourceUname.RingswapV2,
-//         LiquiditySourceUname.UniswapV2,
-//         LiquiditySourceUname.UniswapV3,
-//     ],
-//     [ChainId.SuiMainnet]: [
-//         LiquiditySourceUname.Cetus,
-//     ],
-// };
-
 const notDeployed: LiquiditySourceUname [] = [];
 const revision1: LiquiditySourceUname [] = [
     LiquiditySourceUname.UniswapV2,
